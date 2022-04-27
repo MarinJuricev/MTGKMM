339e05de62f165a7e6b58f2f34d12c96373b7d8a
MarinJ <mjuric00@fesb.hr>
2022-04-28 00:33:20 +0200

    Initial commit

---
+++ common/src/commonMain/kotlin/com/example/mtgkmm/Platform.kt
@@ -0,0 +1,5 @@
+package com.example.mtgkmm
+
+expect class Platform() {
+    val platform: String
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MTGKMM"
+include(":androidApp")
+include(":common")
