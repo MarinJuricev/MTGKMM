a55923f6207f35836f6312706491f547d7d41e4b
MarinJ <mjuric00@fesb.hr>
2022-05-02 00:26:35 +0200

    Add most of the Android presentation level dependencies to Dependencies.kt

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,7 +1,50 @@
 object Versions {
+    const val androidMinSdk = 23
+    const val androidCompileSdk = 32
+    const val androidTargetSdk = androidCompileSdk
+
+    const val koin = "3.1.6"
+
+    const val compose = "1.2.0-alpha08"
+    const val composeCompiler = "1.2.0-alpha08"
+    const val navCompose = "2.4.2"
+    const val coilCompose = "2.0.0-rc03"
+    const val accompanist = "0.23.0"
+
+    const val material = "1.4.0"
+    const val activityCompose = "1.4.0-beta01"
+    const val lifecycleKtx = "2.4.0-rc01"
+    const val lifecycleRuntimeKtx = lifecycleKtx
+    const val lifecycleViewmodelKtx = lifecycleKtx
 
 }
 
 object Dependencies {
 
+    object Compose {
+        const val compiler = "androidx.compose.compiler:compiler:${Versions.composeCompiler}"
+        const val ui = "androidx.compose.ui:ui:${Versions.compose}"
+        const val uiGraphics = "androidx.compose.ui:ui-graphics:${Versions.compose}"
+        const val uiTooling = "androidx.compose.ui:ui-tooling:${Versions.compose}"
+        const val foundationLayout = "androidx.compose.foundation:foundation-layout:${Versions.compose}"
+        const val material = "androidx.compose.material:material:${Versions.compose}"
+        const val navigation = "androidx.navigation:navigation-compose:${Versions.navCompose}"
+
+        const val coilCompose = "io.coil-kt:coil-compose:${Versions.coilCompose}"
+        const val accompanistNavigationAnimation =  "com.google.accompanist:accompanist-navigation-animation:${Versions.accompanist}"
+    }
+
+    object AndroidX {
+        const val lifecycleRuntimeKtx = "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.lifecycleRuntimeKtx}"
+        const val lifecycleViewmodelKtx = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifecycleViewmodelKtx}"
+        const val activityCompose = "androidx.activity:activity-compose:${Versions.activityCompose}"
+    }
+
+    object Koin {
+        const val core = "io.insert-koin:koin-core:${Versions.koin}"
+        const val test = "io.insert-koin:koin-test:${Versions.koin}"
+        const val testJUnit4 = "io.insert-koin:koin-test-junit4:${Versions.koin}"
+        const val android = "io.insert-koin:koin-android:${Versions.koin}"
+        const val compose = "io.insert-koin:koin-androidx-compose:${Versions.koin}"
+    }
 }
