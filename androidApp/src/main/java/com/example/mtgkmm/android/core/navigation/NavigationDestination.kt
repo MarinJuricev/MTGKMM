3a8c7b822bfa9cb77a39301907a8bc38549b29ea
marinjuricev <mjuric00@fesb.hr>
2022-05-31 13:25:06 +0200

    Expand NavigationDestination to include the parcelable helper parser

--- androidApp/src/main/java/com/example/mtgkmm/android/core/navigation/NavigationDestination.kt
+++ androidApp/src/main/java/com/example/mtgkmm/android/core/navigation/NavigationDestination.kt
@@ -2,14 +2,18 @@ package com.example.mtgkmm.android.core.navigation
 
 import androidx.navigation.NamedNavArgument
 import androidx.navigation.NavDeepLink
+import androidx.navigation.NavType
 
 fun interface NavigationDestination<T> {
 
     fun route(): String
     fun buildRoute(data: T): String = route()
+    fun retrieveRouteData(): T? = null
     val arguments: List<NamedNavArgument>
         get() = emptyList()
 
     val deepLinks: List<NavDeepLink>
         get() = emptyList()
+
+    abstract class ParamType<T> : NavType<T>(isNullableAllowed = false)
 }
--- androidApp/src/main/java/com/example/mtgkmm/android/feature/card/navigation/CardDetailDestination.kt
+++ androidApp/src/main/java/com/example/mtgkmm/android/feature/card/navigation/CardDetailDestination.kt
@@ -1,8 +1,12 @@
 package com.example.mtgkmm.android.feature.card.navigation
 
 import android.net.Uri
+import android.os.Bundle
+import androidx.navigation.NamedNavArgument
+import androidx.navigation.navArgument
 import com.example.mtgkmm.android.core.navigation.NavigationDestination
 import com.example.mtgkmm.android.feature.card.model.UiMtgCard
+import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import org.koin.core.component.KoinComponent
@@ -22,6 +26,25 @@ object CardDetailDestination : NavigationDestination<UiMtgCard>, KoinComponent {
     override fun buildRoute(data: UiMtgCard): String =
         "$CARD_DETAIL_ROOT/${Uri.encode(serializer.encodeToString(data))}"
 
-}
+    override val arguments: List<NamedNavArgument>
+        get() = listOf(
+            navArgument(CARD_DETAIL_PARAM) {
+                type = UiMtgCardParamType()
+            }
+        )
 
+    class UiMtgCardParamType : NavigationDestination.ParamType<UiMtgCard>() {
+        override fun get(bundle: Bundle, key: String): UiMtgCard? {
+            return bundle.getParcelable(key)
+        }
+
+        override fun parseValue(value: String): UiMtgCard {
+            return serializer.decodeFromString(value)
+        }
+
+        override fun put(bundle: Bundle, key: String, value: UiMtgCard) {
+            bundle.putParcelable(key, value)
+        }
+    }
+}
 
