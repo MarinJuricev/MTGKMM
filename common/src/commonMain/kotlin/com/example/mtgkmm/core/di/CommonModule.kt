6245dfb59389d54756972fdb3db57266bdb555b4
marinjuricev <mjuric00@fesb.hr>
2022-06-01 14:35:09 +0200

    Separate commonModule into its own file

---
+++ common/src/commonMain/kotlin/com/example/mtgkmm/core/di/CommonModule.kt
@@ -0,0 +1,96 @@
+package com.example.mtgkmm.core.di
+
+import com.example.mtgkmm.feature.search.domain.model.MtgKeyword
+import com.example.mtgkmm.feature.search.domain.model.MtgStat
+import com.squareup.sqldelight.ColumnAdapter
+import io.ktor.client.*
+import io.ktor.client.engine.*
+import io.ktor.client.plugins.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.plugins.logging.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import org.koin.core.qualifier.named
+import org.koin.dsl.module
+
+fun commonModule(enableNetworkLogs: Boolean) = module {
+    single {
+        Json {
+            isLenient = true
+            ignoreUnknownKeys = true
+            explicitNulls = false
+        }
+    }
+
+    single(named(BASE_URL)) {
+        "https://api.scryfall.com/"
+    }
+
+    single {
+        createHttpClient(
+            httpClientEngine = get(),
+            json = get(),
+            enableNetworkLogs = enableNetworkLogs,
+            baseUrl = get(named(BASE_URL))
+        )
+    }
+
+    single<ColumnAdapter<List<MtgKeyword>, String>>(
+        named(MTG_KEYWORD_ADAPTER_NAME)
+    ) {
+        val serializer: Json = get()
+
+        object : ColumnAdapter<List<MtgKeyword>, String> {
+            override fun decode(databaseValue: String) =
+                if (databaseValue.isEmpty()) {
+                    listOf()
+                } else {
+                    serializer.decodeFromString<List<MtgKeyword>>(databaseValue)
+                }
+
+            override fun encode(value: List<MtgKeyword>) =
+                serializer.encodeToString(value)
+        }
+    }
+
+    single<ColumnAdapter<MtgStat, String>>(
+        named(MTG_STAT_ADAPTER_NAME)
+    ) {
+        val serializer: Json = get()
+
+        object : ColumnAdapter<MtgStat, String> {
+            override fun decode(databaseValue: String) =
+                serializer.decodeFromString<MtgStat>(databaseValue)
+
+            override fun encode(value: MtgStat) =
+                serializer.encodeToString(value)
+        }
+    }
+
+}
+
+fun createHttpClient(
+    httpClientEngine: HttpClientEngine,
+    json: Json,
+    enableNetworkLogs: Boolean,
+    baseUrl: String,
+) = HttpClient(httpClientEngine) {
+    install(ContentNegotiation) {
+        json(json)
+    }
+    if (enableNetworkLogs) {
+        install(Logging) {
+            logger = Logger.DEFAULT
+            level = LogLevel.INFO
+        }
+    }
+    defaultRequest {
+        url(baseUrl)
+    }
+}
+
+private const val BASE_URL = "baseUrl"
+const val MTG_KEYWORD_ADAPTER_NAME = "mtgKeywordAdapterName"
+const val MTG_STAT_ADAPTER_NAME = "mtgStatAdapterName"
--- common/src/commonMain/kotlin/com/example/mtgkmm/core/di/InitKoin.kt
+++ common/src/commonMain/kotlin/com/example/mtgkmm/core/di/InitKoin.kt
@@ -1,24 +1,8 @@
-@file:OptIn(ExperimentalSerializationApi::class)
-
 package com.example.mtgkmm.core.di
 
 import com.example.mtgkmm.feature.search.di.searchModule
-import com.example.mtgkmm.feature.search.domain.model.MtgKeyword
-import com.squareup.sqldelight.ColumnAdapter
-import io.ktor.client.*
-import io.ktor.client.engine.*
-import io.ktor.client.plugins.*
-import io.ktor.client.plugins.contentnegotiation.*
-import io.ktor.client.plugins.logging.*
-import io.ktor.serialization.kotlinx.json.*
-import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
 import org.koin.core.context.startKoin
-import org.koin.core.qualifier.named
 import org.koin.dsl.KoinAppDeclaration
-import org.koin.dsl.module
 
 fun initKoin(
     enableNetworkLogs: Boolean = false,
@@ -35,66 +19,3 @@ fun initKoin(
 // Called by non Android consumers
 fun initKoin() = initKoin(enableNetworkLogs = false) {}
 
-fun commonModule(enableNetworkLogs: Boolean) = module {
-    single {
-        Json {
-            isLenient = true
-            ignoreUnknownKeys = true
-            explicitNulls = false
-        }
-    }
-
-    single(named(BASE_URL)) {
-        "https://api.scryfall.com/"
-    }
-
-    single {
-        createHttpClient(
-            httpClientEngine = get(),
-            json = get(),
-            enableNetworkLogs = enableNetworkLogs,
-            baseUrl = get(named(BASE_URL))
-        )
-    }
-
-    single<ColumnAdapter<List<MtgKeyword>, String>> {
-        val serializer: Json = get()
-
-        object : ColumnAdapter<List<MtgKeyword>, String> {
-            override fun decode(databaseValue: String) =
-                if (databaseValue.isEmpty()) {
-                    listOf()
-                } else {
-                    serializer.decodeFromString<List<MtgKeyword>>(databaseValue)
-                }
-
-            override fun encode(value: List<MtgKeyword>) =
-                serializer.encodeToString(value)
-        }
-    }
-
-}
-
-fun createHttpClient(
-    httpClientEngine: HttpClientEngine,
-    json: Json,
-    enableNetworkLogs: Boolean,
-    baseUrl: String,
-) = HttpClient(httpClientEngine) {
-    install(ContentNegotiation) {
-        json(json)
-    }
-    if (enableNetworkLogs) {
-        install(Logging) {
-            logger = Logger.DEFAULT
-            level = LogLevel.INFO
-        }
-    }
-    defaultRequest {
-        url(baseUrl)
-        //client.get<…> { url.appendPath("graphql") } // appended to "https://example.com/foo/"
-        //client.get<…> { url("https://otherdomain.com/foo") } // completely overwrites URL
-    }
-}
-
-private const val BASE_URL = "baseUrl"
