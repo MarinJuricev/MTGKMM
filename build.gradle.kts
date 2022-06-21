293fae86a4c9cd770d8eab1e5648686bb1f11227
marinjuricev <mjuric00@fesb.hr>
2022-06-21 23:48:09 +0200

    Add spotless with the gradle plugin DSL

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,8 @@
+plugins {
+    id("com.diffplug.spotless") version "6.7.2"
+}
+
+//TODO Migrate to plugin DSL
 buildscript {
     val kotlinVersion: String by project
     val gradleVersion: String by project
@@ -6,11 +11,15 @@ buildscript {
         gradlePluginPortal()
         google()
         mavenCentral()
+//        maven {
+//            url = "https://plugins.gradle.org/m2/"
+//        }
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("org.jetbrains.kotlin:kotlin-serialization:${kotlinVersion}")
         classpath("com.android.tools.build:gradle:$gradleVersion")
+//        classpath("com.diffplug.spotless:spotless-plugin-gradle:6.7.2")
 
         with(Dependencies.Gradle) {
             classpath(sqlDelight)
@@ -19,30 +28,15 @@ buildscript {
     }
 }
 
-spotless {
-    // optional: limit format enforcement to just the files changed by this feature branch
-    ratchetFrom 'origin/main'
-
-    format 'misc', {
-    // define the files to apply `misc` to
-    target '*.gradle', '*.md', '.gitignore'
-
-    // define the steps to apply to those files
-    trimTrailingWhitespace()
-    indentWithTabs() // or spaces. Takes an integer argument if you don't like 4
-    endWithNewline()
-}
-    java {
-        // don't need to set target, it is inferred from java
-
-        // apply a specific flavor of google-java-format
-        googleJavaFormat('1.8').aosp().reflowLongStrings()
-        // make sure every file has the following copyright header.
-        // optionally, Spotless can set copyright years by digging
-        // through git history (see "license" section below)
-        licenseHeader '/* (C)$YEAR */'
-    }
-}
+//spotless {
+//    kotlin {
+//        // version, setUseExperimental, userData and editorConfigOverride are all optional
+//        ktlint("0.45.2")
+//            .setUseExperimental(true)
+//            .userData(mapOf("android" to "true"))
+//            .editorConfigOverride(mapOf("indent_size" to 2))
+//    }
+//}
 
 allprojects {
     repositories {
@@ -51,6 +45,6 @@ allprojects {
     }
 }
 
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
-}
+//tasks.register("clean", Delete::class) {
+//    delete(rootProject.buildDir)
+//}
