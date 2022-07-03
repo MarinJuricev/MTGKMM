2909d613d76b5cdc720831a939d2b486bf7f43d0
marinjuricev <mjuric00@fesb.hr>
2022-07-03 22:43:33 +0200

    Play around with spotless integration

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,23 +24,45 @@ buildscript {
     }
 }
 
-configure<com.diffplug.gradle.spotless.SpotlessExtension> {
-    kotlin {
-        // version, setUseExperimental, userData and editorConfigOverride are all optional
-        ktlint("0.45.2")
-            .setUseExperimental(true)
-            .userData(mapOf("android" to "true"))
-            .editorConfigOverride(mapOf("indent_size" to 4))
-    }
-    kotlinGradle {
-        target("*.gradle.kts") // default target for kotlinGradle
-        ktlint() // or ktfmt() or prettier()
-    }
-}
+//configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+//    kotlin {
+//        // version, setUseExperimental, userData and editorConfigOverride are all optional
+//        ktlint("0.45.2")
+//            .setUseExperimental(true)
+//            .userData(mapOf("android" to "true"))
+//            .editorConfigOverride(mapOf("indent_size" to 4))
+//    }
+//    kotlinGradle {
+//        target("*.gradle.kts") // default target for kotlinGradle
+//        ktlint() // or ktfmt() or prettier()
+//    }
+//}
 
 allprojects {
     repositories {
         google()
         mavenCentral()
+    }
+}
+
+subprojects {
+    apply(plugin = "com.diffplug.spotless")
+    configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+        kotlin {
+            target ("**/*.kt")
+            ktlint().userData(mapOf("indent_size" to "2", "continuation_indent_size" to "2"))
+            ktfmt().googleStyle()
+            licenseHeaderFile("${project.rootProject.projectDir}/license-header.txt")
+        }
+
+        kotlinGradle {
+            target("*.gradle.kts") // default target for kotlinGradle
+            ktlint() // or ktfmt() or prettier()
+        }
+
+        format ("xml") {
+            target ("**/*.xml")
+            prettier(mapOf("prettier" to "2.0.5", "@prettier/plugin-xml" to "0.13.0")).config(mapOf("parser" to "xml", "tabWidth" to 4))
+        }
     }
 }
