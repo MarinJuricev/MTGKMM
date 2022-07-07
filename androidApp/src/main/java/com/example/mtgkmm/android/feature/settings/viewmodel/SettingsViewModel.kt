9f75ea37fa6ed59bb8d1edddb9af2f93200f9266
marinjuricev <mjuric00@fesb.hr>
2022-07-07 09:00:43 +0200

    Fix spotless issues

--- androidApp/src/main/java/com/example/mtgkmm/android/feature/settings/viewmodel/SettingsViewModel.kt
+++ androidApp/src/main/java/com/example/mtgkmm/android/feature/settings/viewmodel/SettingsViewModel.kt
@@ -2,17 +2,16 @@ package com.example.mtgkmm.android.feature.settings.viewmodel
 
 import com.example.mtgkmm.android.core.BaseViewModel
 import com.example.mtgkmm.android.feature.settings.model.SettingsEvent
-import com.example.mtgkmm.android.feature.settings.model.SettingsEvent.*
 
 class SettingsViewModel(
     private val observeSettingsData: ObserveSettingsData,
-    private val setSortType: SetSortType,
+    private val setSortType: SetSortType
 ) : BaseViewModel<SettingsEvent>() {
 
     override fun onEvent(event: SettingsEvent) {
         when (event) {
-            is OnChangeSortType -> TODO()
-            OnGetSettingsData -> TODO()
+            is SettingsEvent.OnChangeSortType -> TODO()
+            SettingsEvent.OnGetSettingsData -> TODO()
         }
     }
 }
